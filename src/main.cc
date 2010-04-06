9681e352b0bf50ba8f750c92111a5df432dd9f5b
Christopher Bertels <bakkdoor@flasht.de>
2010-04-07 00:01:33 +0200

    fancy now reads from stdin again, when no filename is given as parameter (as it did in the beginning before adding the core library files in the lib/ directory).

--- src/main.cc
+++ src/main.cc
@@ -3,21 +3,24 @@
 
 /* prototype of bison-generated parser function */
 int yyparse();
+int yyrestart(FILE*);
 extern int yylineno;
 extern string current_file;
 
 void parse_file(string &filename)
 {
-  if(freopen(filename.c_str(), "r", stdin) == NULL)
+  FILE *file = fopen(filename.c_str(), "r");
+  if(file == NULL)
   {
     fprintf(stderr, "File %s cannot be opened.\n", filename.c_str());
     exit(1);
   }
   
   current_file = filename;
-  
+  yyrestart(file);
   yyparse();
   yylineno = 1; // reset yylineno for next file to parse
+  fclose(file);
 }
 
 int main(int argc, char **argv)
@@ -46,16 +49,15 @@ int main(int argc, char **argv)
   for(unsigned int i = 0; i < files_vector.size(); i++) {
     parse_file(files_vector[i]);
   }
-  
-  if ((argc > 1) && (freopen(argv[1], "r", stdin) == NULL))
-  {
-    fprintf(stderr, "%s: File %s cannot be opened.\n", argv[0], argv[1]);
-    exit(1);
-  }
-  
-  try {
-    current_file = string(argv[1]);
-    yyparse();
+
+  try {  
+    if (argc > 1) {
+      string filename = string(argv[1]);
+      parse_file(filename);
+    } else {
+      yyrestart(stdin);
+      yyparse();
+    }
   } catch(UnknownIdentifierError &ex) {
     cout << "Error:" << endl;
   }
