aaec75e63371815c4f39a86e4df9cd834e259c16
Christopher Bertels <bakkdoor@flasht.de>
2010-04-16 21:22:22 +0200

    Added documentation to ExpressionList & FancyException

--- src/fancy_exception.h
+++ src/fancy_exception.h
@@ -3,12 +3,37 @@
 
 namespace fancy {
 
+  /**
+   * Class that represents Exceptions within Fancy.
+   */
   class FancyException : public FancyObject
   {
   public:
+    /**
+     * Initializes an Exception of a given Exception Class.
+     * @param Exception_class The (runtime) Class of the Exception.
+     */
     FancyException(Class_p Exception_class);
+
+    /**
+     * Initializes an Exception with a given message and an Exception Class.
+     * @param message Message of the Exception.
+     * @param Exception_class The (runtime) Class of the Exception.
+     */
     FancyException(const string &message, Class_p Exception_class);
+
+    /**
+     * Initializes an Exception with a given message.
+     * Its runtime Class will be ExceptionClass.
+     * @param message Message of the Exception.
+     */
     FancyException(const string &message);
+
+    /**
+     * Initializes an Exception with a given exception_value and a message.
+     * @param exception_value Value representing the Exception.
+     * @param message Message of the Exception.
+     */
     FancyException(FancyObject_p exception_value, const string &message);
 
     virtual ~FancyException();
@@ -17,8 +42,23 @@ namespace fancy {
     virtual OBJ_TYPE type() const;
     virtual string to_s() const;
 
+    /**
+     * Returns the message of the Exception.
+     * @return Message of the Exception (can be an empty String if not defined).
+     */
     string message() const;
+
+    /**
+     * Returns the (runtime) Class of the Exception.
+     * E.g. MethodNotFoundError, IOError etc.
+     * @return (Runtime) Class of the Exception.
+     */
     Class_p exception_class() const;
+
+    /**
+     * Returns the Exception's value.
+     * @return The Exception's value or 'this' (if not set).
+     */
     FancyObject_p exception_value();
 
   private:
