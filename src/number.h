2eb4c5413a4d069a10164270da962e68ff33b5e2
Christopher Bertels <bakkdoor@flasht.de>
2010-05-12 16:23:27 +0200

    added documentation for Number & Regex classes.

--- src/number.h
+++ src/number.h
@@ -8,24 +8,68 @@ namespace fancy {
 
   #define INT_CACHE_SIZE 1000
 
+  /**
+   * Number class representing Number objects (both double and
+   * integer) in Fancy.
+   */
   class Number : public FancyObject
   {
   public:
+    /**
+     * Number constructor for double value.
+     * @param value Double value for Number.
+     */
     Number(double value);
+
+    /**
+     * Number constructor for int value.
+     * @param value Integer value for Number.
+     */
     Number(int value);
     ~Number();
-  
+
+    /**
+     * See FancyObject for these methods.
+     */  
     virtual FancyObject_p equal(const FancyObject_p other) const;
     virtual OBJ_TYPE type() const;
     virtual string to_s() const;
 
+    /**
+     * Indicates, if Number is double.
+     * @return true if double, false otherwise.
+     */
     bool is_double() const;
+
+    /**
+     * Returns the double value (casted, if needed).
+     * @return Double value for the Number.
+     */
     double doubleval() const;
+
+    /**
+     * Returns the int value (casted, if needed).
+     * @return Integer value for the Number.
+     */
     int intval() const;
   
+    /**
+     * Returns Number object with the given int value.
+     * @param value Integer value for the Number object.
+     * @return Number object with the given value.
+     */
     static Number_p from_int(int value);
+
+    /**
+     * Returns Number object with the given double value.
+     * @param value Double value for the Number object.
+     * @return Number object with the given value.
+     */
     static Number_p from_double(double value);
 
+    /**
+     * Initializes the number cache with the first INT_CACHE_SIZE integer Numbers.
+     */
     static void init_cache();
 
   private:
--- src/regex.h
+++ src/regex.h
@@ -3,16 +3,39 @@
 
 namespace fancy {
 
+  /**
+   * Regexp class representing Regular Expression Objects in Fancy.
+   */
   class Regex : public FancyObject
   {
   public:
+    /**
+     * Regex constructor.
+     * @param pattern A C++ string that is the regexp pattern.
+     */
     Regex(const string &pattern);
     ~Regex();
-  
+
+    /**
+     * See FancyObject for these methods.
+     */  
     virtual FancyObject_p equal(const FancyObject_p other) const;
     virtual OBJ_TYPE type() const;
     virtual string to_s() const;
+
+    /**
+     * Returns the pattern string.
+     * @return C++ pattern string.
+     */
     string pattern() const;
+
+    /**
+     * Does the matching on a given string.
+     * @param string The string to match this regular expression
+     * against.
+     * @return The match value (e.g. nil, if none match or the array
+     * with matches).
+     */
     FancyObject_p match(String_p string) const;
 
   private:
