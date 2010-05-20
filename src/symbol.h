49b8458c29bc93b00bf46286d4282a27ed804786
Christopher Bertels <bakkdoor@flasht.de>
2010-05-21 00:37:03 +0200

    src/symbol.h: Added documentation for Symbol class.

--- src/symbol.h
+++ src/symbol.h
@@ -6,18 +6,43 @@ namespace fancy {
   class Symbol;
   typedef Symbol* Symbol_p;
 
+  /**
+   * Symbol class representing Symbol objects in Fancy.
+   * A Symbol is an identifier, starting with a colon (':').
+   * Examples: :foo, :bar, :baz
+   */
   class Symbol : public FancyObject
   {
   public:
+    /**
+     * Symbol constructor.
+     * @param name Name of the Symbol.
+     */
     Symbol(const string &name);
     ~Symbol();
   
+    /**
+     * See FancyObject for these methods.
+     */
     virtual FancyObject_p equal(const FancyObject_p other) const;
     virtual OBJ_TYPE type() const;
     virtual string to_s() const;
     virtual string inspect() const;
+
+    /**
+     * Returns the name of the Symbol (as a C++ string).
+     * @return The name of the Symbol (as a C++ string).
+     */
     string name() const;
 
+    /**
+     * Returns a Symbol object with a given name.
+     * Symbols in Fancy get cached, since they're immutable.
+     * Multiple occurances of the same Symbol in Fancy use the same
+     * Symbol object.
+     * @param value C++ string for the name.
+     * @return A Symbol object with a given name.
+     */
     static Symbol_p from_string(const string &name);
 
   private:
