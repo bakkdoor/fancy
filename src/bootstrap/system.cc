1bb124e4abde7ac37c0bc6844b151edd49dbce8d
Christopher Bertels <bakkdoor@flasht.de>
2010-05-26 18:28:42 +0200

    src/bootstrap/system.cc: Added System#pipe: method. It works similar to popen() in C, but for now only returns an Array of Strings that make up the lines of the output of the popen() call.

--- src/bootstrap/system.cc
+++ src/bootstrap/system.cc
@@ -14,6 +14,11 @@ namespace fancy {
                       "do:",
                       "Runs the given string as a system() command.",
                       do);
+
+      DEF_CLASSMETHOD(SystemClass,
+                      "pipe:",
+                      "Runs the given string as a popen() call and returns the output of the call as a string.",
+                      pipe);
     }
 
     CLASSMETHOD(SystemClass, exit)
@@ -24,8 +29,29 @@ namespace fancy {
 
     CLASSMETHOD(SystemClass, do)
     {
+      EXPECT_ARGS("System##do:", 1);
       system(args[0]->to_s().c_str());
       return nil;
+    }
+
+    CLASSMETHOD(SystemClass, pipe)
+    {
+      // TODO: implement this using Files instead of returning an Array
+
+      EXPECT_ARGS("System##pipe:", 1);
+      FILE *f = popen(args[0]->to_s().c_str(), "r");
+      vector<FancyObject_p> lines;
+      while(!feof(f)) {
+        stringstream line;
+        char c;
+        while((c = fgetc(f)) && c != EOF && c != '\n') {
+          line << c;
+        }
+        if(c != EOF) {
+          lines.push_back(new String(line.str()));
+        }
+      }
+      return new Array(lines);
     }
 
   }
