fcb5883d52d8e8cf23197af482f8c71eb0c0ae93
Christopher Bertels <bakkdoor@flasht.de>
2010-05-26 17:22:09 +0200

    src/bootstrap/system.cc: Added System##do: method (similar to Ruby's Kernel#system() method) to run a command in a shell.

--- src/bootstrap/system.cc
+++ src/bootstrap/system.cc
@@ -9,11 +9,23 @@ namespace fancy {
                       "exit",
                       "Exit the running Fancy process.",
                       exit);
+
+      DEF_CLASSMETHOD(SystemClass,
+                      "do:",
+                      "Runs the given string as a system() command.",
+                      do);
     }
 
     CLASSMETHOD(SystemClass, exit)
     {
       exit(0);
+      return nil;
+    }
+
+    CLASSMETHOD(SystemClass, do)
+    {
+      system(args[0]->to_s().c_str());
+      return nil;
     }
 
   }
