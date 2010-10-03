c3c5f5cd5be6b8076f9c0a5b47bb7bad220dc827
Christopher Bertels <bakkdoor@flasht.de>
2010-10-03 02:50:10 +0200

    Threw out boilerplate code. Simply use rubinius' built in node classes if they are all we actually need.

--- rbx/compiler/ast/number_literal.rb
+++ rbx/compiler/ast/number_literal.rb
@@ -1,30 +1,6 @@
 module Fancy
   module AST
-
-    class IntLiteral < Node
-      name :int_lit
-
-      def initialize(line, val)
-        super(line)
-        @val = val
-      end
-
-      def bytecode(g)
-        Rubinius::AST::FixnumLiteral.new(0, @val).bytecode(g)
-      end
-    end
-
-    class DoubleLiteral < Node
-      name :double_lit
-      def initialize(line, val)
-        super(line)
-        @val = val
-      end
-
-      def bytecode(g)
-        Rubinius::AST::FloatLiteral.new(0, @val).bytecode(g)
-      end
-    end
-
+    Nodes[:int_lit] = Rubinius::AST::FixnumLiteral
+    Nodes[:double_lit] = Rubinius::AST::FloatLiteral
   end
 end
--- rbx/compiler/ast/string_literal.rb
+++ rbx/compiler/ast/string_literal.rb
@@ -1,23 +1,5 @@
 module Fancy
   module AST
-
-    class StringLiteral < Node
-      name :string_lit
-
-      def initialize(line, string)
-        super(line)
-        @string = string
-      end
-
-      def bytecode(g)
-        # pos(g)
-        # # TODO: change to push_unique_literal
-        # g.push_literal @string
-        # g.string_dup
-        Rubinius::AST::StringLiteral.new(0, @string).bytecode(g)
-      end
-
-    end
-
+    Nodes[:string_lit] = Rubinius::AST::StringLiteral
   end
 end
