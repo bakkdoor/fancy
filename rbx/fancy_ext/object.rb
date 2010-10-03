b421fcc52c3931ac9dcd98bbe0675bf20ec46d4b
Victor Hugo Borja <vborja@apache.org>
2010-10-03 10:19:02 -0500

    Operator ++ defined on object as it converts anything to string and then concats

--- rbx/fancy_ext/object.rb
+++ rbx/fancy_ext/object.rb
@@ -2,4 +2,10 @@ class Object
   def println
     puts self
   end
+
+  def fancy_concat(other)
+    self.to_s + other.to_s
+  end
+
+  alias_method :"++", :fancy_concat
 end
--- rbx/fancy_ext/string.rb
+++ rbx/fancy_ext/string.rb
@@ -1,5 +1,2 @@
 class String
-  define_method("++") do |other|
-    self + other.to_s
-  end
 end
