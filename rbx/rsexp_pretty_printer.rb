7815ad2178f7d75ababf9c88f21b3230cace9e24
Nils Haldenwang <nohfreakz@web.de>
2010-10-08 10:08:24 +0200

    Turned the pretty printer script into a class, to make it reusable.

--- rbx/rsexp_pretty_printer.rb
+++ rbx/rsexp_pretty_printer.rb
@@ -1,56 +1,76 @@
 #!/usr/bin/env rbx
 # -*- ruby -*-
 
-a = eval ARGV[0]
+module Fancy
+  class RSexpPrettyPrinter
 
-def red(str)
-  "\e[1;31m" + str + "\e[0m"
-end
+    def nice(to_print)
 
-def green(str)
-  "\e[32m" + str + "\e[0m"
-end
-
-def print_indented(to_print, lvl=0, comma=false)
-
-   if(to_print.is_a? Array)
-     puts
-     print "  " * lvl
-     print "["
-     print red ":" + to_print.first.to_s if !to_print.empty?
-
-     print ", " if to_print.size > 1
-
-     1.upto to_print.size do |i|
-      if i < (to_print.size - 1)
-        print_indented to_print[i], lvl+1, true
-      else
-        print_indented to_print[i], lvl+1
+      if to_print.is_a? String
+        print_indented eval(to_print)
+      elsif to_print.is_a? Array
+        print_indented to_print
       end
-     end
 
-     print "]"
-     print "," if comma
+      puts
 
-
-  elsif(to_print)
-    puts
-    print "  " * lvl
-
-    if(to_print.is_a? Symbol)
-      print ":"
-      print green to_print.to_s
-    elsif to_print.is_a? String
-      print "'"
-      print green to_print.to_s
-      print "'"
-    else
-      print green to_print.to_s
     end
 
-   end
+    def red(str)
+      "\e[1;31m" + str + "\e[0m"
+    end
+
+    def green(str)
+      "\e[32m" + str + "\e[0m"
+    end
+
+    def print_indented(to_print, lvl=0, comma=false)
+
+      if(to_print.is_a? Array)
+        puts
+        print "  " * lvl
+        print "["
+        print red ":" + to_print.first.to_s if !to_print.empty?
+
+        print ", " if to_print.size > 1
+
+        1.upto to_print.size do |i|
+          if i < (to_print.size - 1)
+            print_indented to_print[i], lvl+1, true
+          else
+            print_indented to_print[i], lvl+1
+          end
+        end
+
+        print "]"
+        print "," if comma
+
+
+      elsif(to_print)
+        puts
+        print "  " * lvl
+
+        if(to_print.is_a? Symbol)
+          print ":"
+          print green to_print.to_s
+        elsif to_print.is_a? String
+          print "'"
+          print green to_print.to_s
+          print "'"
+        else
+          print green to_print.to_s
+        end
+
+      end
+    end
+
+  end
+
 end
 
-print_indented a
-puts
-puts
+
+if __FILE__ == $0
+  p = Fancy::RSexpPrettyPrinter.new
+  p.nice ARGV[0]
+end
+
