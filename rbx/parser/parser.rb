ef8eb3bf00f394b2791e69370bafc931086ca581
Victor Hugo Borja <vborja@apache.org>
2010-10-14 04:24:44 -0500

    Added usage comments on parser.rb

--- rbx/parser/parser.rb
+++ rbx/parser/parser.rb
@@ -1,9 +1,19 @@
+## This file expects rbx/compiler/ast has been loaded
+
+
 module Fancy
 
+  # This module has methods that can be used as callbacks from
+  # the parser, so that we can have a relatively simple fancy.y
+  # For example, when the parser sees a literal (or any other node)
+  # it just calls methods defined here. that create actual
+  # Fancy::AST nodes, returning them to the parser to be stored on
+  # $$
   module Parser
 
-    def self.show(object)
-      p object
+    # Invoked by the parser when it has seen an string
+    def create_integer(line, token)
+
     end
 
   end
