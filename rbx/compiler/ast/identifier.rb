a4ef29c3152b2ca390a5df92fc9b637bbdcab5f8
Christopher Bertels <bakkdoor@flasht.de>
2010-10-15 10:56:28 +0200

    Changed @identifier back for class_variable since it caused errors.

--- rbx/compiler/ast/identifier.rb
+++ rbx/compiler/ast/identifier.rb
@@ -47,7 +47,7 @@ module Fancy
         if constant?
           @identifier.to_sym
         elsif class_variable?
-          @identifier[2..-1].to_sym
+          @identifier.to_sym
         elsif instance_variable?
           @identifier[1..-1].to_sym
         else
