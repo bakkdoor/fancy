04a3135831cf859ba6ae19c8618593f3bad9105b
Christopher Bertels <bakkdoor@flasht.de>
2010-10-20 18:55:24 +0200

    Commented out output of which file is comiling in rbx/compiler/command.rb

--- rbx/compiler/command.rb
+++ rbx/compiler/command.rb
@@ -4,7 +4,7 @@ module Fancy
       def self.run(argv)
         file = ARGV.shift
         raise "Expected filename as single argument." unless file
-        puts "Compiling #{file}"
+        # puts "Compiling #{file}"
         Rubinius::Compiler.compile_fancy_file file, nil, 1, ARGV.include?("-B")
       end
     end
