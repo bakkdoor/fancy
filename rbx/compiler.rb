bebe1b8ecdaf9749f48dd37fea8c8a3548155a15
Christopher Bertels <bakkdoor@flasht.de>
2010-10-24 19:42:15 +0200

    rbx/compiler.rb: Typo fix.

--- rbx/compiler.rb
+++ rbx/compiler.rb
@@ -4,7 +4,7 @@
 #
 # This compiler is written in Ruby, because it's easier to start
 # writing a working compiler with the given tools of Rubinius that are
-# written in Fancy than porting all that code to Fancy.
+# written in Ruby than porting all that code to Fancy.
 #
 # Once we can compile Fancy to Rubinius we will reimplement the
 # compiler in Fancy and can still reuse the rubinius compiler
