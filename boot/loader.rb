f6e7520ff5258e07f7cb7bf1b32efc9576691efa
Christopher Bertels <bakkdoor@flasht.de>
2010-12-21 23:35:02 +0100

    Experimenting with getting fdoc to work. Not done yet.

---
+++ boot/loader.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env rbx
+
+base = File.dirname(__FILE__) + "/"
+
+# load in fancy specific extensions
+require base + "fancy_code_loader"
+require base + "fancy_ext"
+
+if $0 == __FILE__
+  # load & run file
+  file = ARGV.shift
+  raise "Expected a fancy file to load" unless file
+  if File.exists? file
+    Fancy::CodeLoader.load_compiled_file file
+  else
+    Fancy::CodeLoader.load_compiled_file(base + "../lib/main.fyc")
+  end
+end
