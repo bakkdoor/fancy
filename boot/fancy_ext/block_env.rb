560e07ab3fd35ef21ff3a73ce7890a4de2678ae7
Christopher Bertels <cbertels@backtype.com>
2011-02-15 21:51:21 -0800

    Check argument size passed to Block#call:. Raise ArgumentError if given too few arguments and only use the amount needed when passed in too many.

--- boot/fancy_ext/block_env.rb
+++ boot/fancy_ext/block_env.rb
@@ -12,10 +12,15 @@ class Block
   alias_method :":call", :call
 
   define_method("call:") do |args|
-    if args.first.is_a?(Array) && args.size == 1
-      call args.first
+    if args.size < self.arity
+      raise ArgumentError, "Too few arguments for block: #{args.size} - Minimum of #{self.arity} expected"
     else
-      call *args
+      if args.first.is_a?(Array) && args.size == 1
+        call args.first
+      else
+        args = args.first(self.arity) if args.size > self.arity
+        call *args
+      end
     end
   end
 
