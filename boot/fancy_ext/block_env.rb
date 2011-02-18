012385985fedb8afe4096c5e748626dcf4aac7c9
Christopher Bertels <bakkdoor@flasht.de>
2011-02-18 04:24:59 -0800

    Added check for type of args to Block#call: (expect an Array and fail otherwise).

--- boot/fancy_ext/block_env.rb
+++ boot/fancy_ext/block_env.rb
@@ -12,6 +12,9 @@ class Block
   alias_method :":call", :call
 
   define_method("call:") do |args|
+    unless args.is_a? Array
+      raise ArgumentError, "Expecting Array of arguments for Block"
+    end
     if args.size < self.arity
       raise ArgumentError, "Too few arguments for block: #{args.size} - Minimum of #{self.arity} expected"
     else
