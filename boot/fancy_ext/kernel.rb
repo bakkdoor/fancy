2035bef49cbb24b453c8b6ddd83de9ae56a35fc4
Christopher Bertels <bakkdoor@flasht.de>
2011-04-14 18:38:02 -0700

    Add compatibility for older Rubinius versions (wrt metaclass method) and use singleton_class if that fails.

--- boot/fancy_ext/kernel.rb
+++ boot/fancy_ext/kernel.rb
@@ -1,7 +1,10 @@
 module Kernel
-
-  alias_method :":metaclass", :singleton_class
-  alias_method :metaclass, :singleton_class
+  begin
+    alias_method ":metaclass", :metaclass
+  rescue
+    alias_method :":metaclass", :singleton_class
+    alias_method :metaclass, :singleton_class
+  end
 
   def fancy_require(file, compile = false)
     if compile
