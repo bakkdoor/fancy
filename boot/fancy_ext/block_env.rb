ba395276365910fe1fde4cda2c45a3890f71eb2b
Christopher Bertels <bakkdoor@flasht.de>
2011-05-04 04:42:39 -0700

    Remove argument checking for Block#call: to speed up loops. This also allows any collection type to be used instead of Arrays, if needed.

--- boot/fancy_ext/block_env.rb
+++ boot/fancy_ext/block_env.rb
@@ -6,14 +6,6 @@ class Block
   alias_method :":call", :call
 
   define_method("call:") do |args|
-    unless args.is_a? Array
-      raise ArgumentError, "Expecting Array of arguments for Block"
-    end
-    if args.size < self.arity
-      raise ArgumentError, "Too few arguments for block: #{args.size} - Minimum of #{self.arity} expected"
-    else
-      args = args.first(self.arity) if args.size > self.arity
-      call *args
-    end
+    call *args
   end
 end
