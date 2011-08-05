c407824ab4a01a22bb557882aa747373a6dbfd27
Christopher Bertels <bakkdoor@flasht.de>
2011-08-05 14:09:41 -0700

    Add Self#method_name to bootstrap compiler.

--- boot/rbx-compiler/compiler/ast/node.rb
+++ boot/rbx-compiler/compiler/ast/node.rb
@@ -4,3 +4,13 @@ class Fancy
       each { |n| const_set(n, Rubinius::AST.const_get(n)) }
   end
 end
+
+class Rubinius::AST::Self
+  def method_name(receiver = nil, with_ruby_args = false)
+    if with_ruby_args
+      "self".to_sym
+    else
+      ":self".to_sym
+    end
+  end
+end
