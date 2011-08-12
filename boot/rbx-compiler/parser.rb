cc23e908276bdf87c1fee303250cde45a78d3203
Victor Hugo Borja <vic.borja@gmail.com>
2011-08-12 15:00:23 -0500

    Removed old bootstrap c-ext parser.

--- boot/rbx-compiler/parser.rb
+++ boot/rbx-compiler/parser.rb
@@ -1,3 +1,1 @@
-require File.expand_path("parser/parser", File.dirname(__FILE__))
-#require File.expand_path("parser/fancy_parser.so", File.dirname(__FILE__))
 require File.expand_path("parser/kpeg_parser", File.dirname(__FILE__))
